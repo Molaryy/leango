486b9e9dc4303354d97dd79cf9d1aec7db877f96
Molaryy <molaryyios@gmail.com>
2026-01-01 16:50:34 +0900

    feat: added arguments handling for better multi-file project approach from the beginning

--- main.go
+++ main.go
@@ -3,18 +3,11 @@ package main
 import (
 	"fmt"
 	logger "github.com/sirupsen/logrus"
-	"leango/Logger"
-	"leango/Token"
-	"log"
+	arguments "leango/src/Args"
+	"leango/src/Token"
 	"os"
 	"strings"
 )
-
-func checkErr(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
 
 func Split(r rune) bool {
 	return r == ' ' || r == '\t'
@@ -25,7 +18,10 @@ func parseFile(filePath string) {
 	var tokenFunctions map[string]func([]string, int) = Token.Handler()
 	data, err := os.ReadFile(filePath)
 
-	checkErr(err)
+	if err != nil {
+		fmt.Println(fmt.Errorf("%v", err))
+		return
+	}
 	lines := strings.Split(string(data), "\n")
 
 	for idxLine, line := range lines {
@@ -34,7 +30,7 @@ func parseFile(filePath string) {
 			for idx, token := range tokens {
 				_, varNameExists := tokenFunctions[token]
 				if idx > 0 && varNameExists {
-					Logger.Fatal(idxLine, "you can't name a variable with the same name as a type")
+					logger.Fatal(idxLine, "you can't name a variable with the same name as a type")
 				}
 				if Token.IsTokenAvailable(token) {
 					tokenFunctions[token](tokens, idx+1)
@@ -45,13 +41,36 @@ func parseFile(filePath string) {
 	Token.ShowVariables()
 }
 
-func main() {
-	if len(os.Args) != 2 {
-		log.Default().Fatal("not enough arguments\n./leango [filepath]")
+func checkFileSuffix(filename string) bool {
+	if !strings.HasSuffix(filename, ".leango") {
+		logger.WithFields(logger.Fields{}).Fatal("incorrect file extension, should match .leango")
+		return false
 	}
-	if !strings.HasSuffix(os.Args[1], ".leango") {
-		logger.WithFields(logger.Fields{}).Fatal(fmt.Sprintf("incorrect file extension [%s]", os.Args[1]))
-	}
+	return true
+}
 
-	parseFile(os.Args[1])
+func main() {
+	existingFlags := []arguments.Flag{
+		{
+			Name:         "--debug",
+			Shorthand:    "-d",
+			ExpectsValue: false,
+			Description:  "debugging mode is enabled for leango",
+		},
+	}
+	args := os.Args[1:]
+	arguments, err := arguments.GetArguments(existingFlags, args)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	flags := arguments.Flags
+	files := arguments.Files
+
+
+	fmt.Println(flags)
+	fmt.Println(files)
+
+	/*if checkFileSuffix(os.Args[1]) {
+	    parseFile(os.Args[1])
+	}*/
 }
