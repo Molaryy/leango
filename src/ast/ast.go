59dc55b16d0c725393b9ff13b517cced09e01b5f
Molaryy <molaryyios@gmail.com>
2026-01-02 11:52:56 +0900

    feat: started AST implementation for the interpreter

---
+++ src/ast/ast.go
@@ -0,0 +1,49 @@
+package ast
+
+import "leango/src/Token"
+
+type ASTNode struct {
+	Parent *ASTNode
+	Children *ASTNode
+	Next *ASTNode
+	Token Token.Token
+}
+
+func AddNewChildren(token Token.Token, ast *ASTNode) *ASTNode {
+	newNode := &ASTNode{
+		Token: token,
+		Parent: nil,
+		Children: nil,
+		Next: nil,
+	}
+
+	if ast == nil {
+		return newNode
+	}
+	newNode.Parent = ast
+	ast.Children = newNode
+	return ast
+}
+
+func AddToExistingChildren(token Token.Token, ast *ASTNode) *ASTNode {
+	newNode := &ASTNode{
+		Token: token,
+		Parent: nil,
+		Children: nil,
+		Next: nil,
+	}
+
+	if ast == nil {
+		return newNode
+	}
+	tmp := ast
+
+	for ast.Next != nil {
+		ast = ast.Next
+	}
+	newNode.Parent = tmp
+	ast.Next = newNode
+	ast = tmp
+	return ast
+}
+
