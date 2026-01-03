c85441a32f942b562c12861aa76cf66aeecfb4b4
Molaryy <molaryyios@gmail.com>
2026-01-03 17:39:20 +0900

    feat: added helper function

---
+++ pkg/helper/helper.go
@@ -0,0 +1,11 @@
+package helper
+
+import "fmt"
+
+
+func Helper() {
+	fmt.Println("Leango is a tool for managing leango source code.\n")
+	fmt.Println("Usage\n\n\tleango [options] file...")
+	fmt.Println("OPTIONS:")
+	fmt.Println("--debug\tdebugging mode is enabled for leango")
+}
