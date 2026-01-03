2b0d98c72c83f0a506382533ce11d360ddde1505
Molaryy <molaryyios@gmail.com>
2026-01-03 17:39:06 +0900

    feat: added debugger log colors + removed log package

--- pkg/debugger/debugger.go
+++ pkg/debugger/debugger.go
@@ -1,10 +1,18 @@
 package debugger
 
 import (
+	"fmt"
 	arguments "leango/src/Args"
 	"leango/src/Token"
-	"log"
 	"time"
+)
+
+const (
+	RESET = "\033[0m"
+	BLUE  = "\033[34m"
+    RED   = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
 )
 
 func IsDebugActivated(flags map[string]arguments.Flag) bool {
@@ -17,7 +25,20 @@ func IsDebugActivated(flags map[string]arguments.Flag) bool {
 }
 
 func PrintToken(token Token.Token) {
-	log.Printf("%+v",token)
+	str := fmt.Sprintf("Type: %s ", token.Type)
+	switch v := token.Value.(type) {
+    case int:
+        str += fmt.Sprintf("Value: %d\n", v)
+    case string:
+        str += fmt.Sprintf("Value: %s\n", v)
+    case bool:
+        str += fmt.Sprintf("Value: %t\n", v)
+	case byte:
+		str += fmt.Sprintf("Value: %c\n", v)
+    default:
+        str += fmt.Sprintf("Unknown type: %T\n", v)
+    }
+	fmt.Printf(str)
 }
 
 func PrintTokenAndSleep(token Token.Token, seconds int64) {
