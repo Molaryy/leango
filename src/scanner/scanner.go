0fa19ffcff9b35ef072bf60e74d559d4815a401c
Molaryy <molaryyios@gmail.com>
2026-01-07 11:08:38 +0900

    feat: scanner now supports keywords

--- src/scanner/scanner.go
+++ src/scanner/scanner.go
@@ -4,43 +4,43 @@ import (
 	"fmt"
 	"leango/pkg/debugger"
 	arguments "leango/src/Args"
-	"leango/src/Token"
+	"leango/src/token"
 )
 
-func scanDelimiterAndOperator(b byte) (Token.Token, bool) {
-	var tok Token.Token
+func scanDelimiterAndOperator(b byte) (token.Token, bool) {
+	var tok token.Token
 	found := false
 
 	switch b {
-		case '{':
-			tok = Token.Token{Type: "DELIMITER_OPEN_BRACE", Value: b}
-		case '}':
-			tok = Token.Token{Type: "DELIMITER_CLOSE_BRACE", Value: b}
-		case '[':
-			tok = Token.Token{Type: "DELIMITER_OPEN_BRACKET", Value: b}
-		case ']':
-			tok = Token.Token{Type: "DELIMITER_CLOSE_BRACKET", Value: b}
-		case '(':
-			tok = Token.Token{Type: "DELIMITER_OPEN_PARENTHESES", Value: b}
-		case ')':
-			tok = Token.Token{Type: "DELIMITER_CLOSE_PARENTHESES", Value: b}
-		case ';':
-			tok = Token.Token{Type: "DELIMITER_SEMICOLON", Value: b}
-		case '=':
-			tok = Token.Token{Type: "OPERATOR_ASSIGN", Value: b}
-		case '+':
-			tok = Token.Token{Type: "OPERATOR_ADDITION", Value: b}
-		case '-':
-			tok = Token.Token{Type: "OPERATOR_SUBTRACTION", Value: b}
-		case '*':
-			tok = Token.Token{Type: "OPERATOR_MULTIPLICATION", Value: b}
-		case '/':
-			tok = Token.Token{Type: "OPERATOR_DIVISION", Value: b}
-		}
-		if tok.Type != "" {
-			found = true
-		}
-		return tok, found
+	case '{':
+		tok = token.Token{Type: "DELIMITER_OPEN_BRACE"}
+	case '}':
+		tok = token.Token{Type: "DELIMITER_CLOSE_BRACE"}
+	case '[':
+		tok = token.Token{Type: "DELIMITER_OPEN_BRACKET"}
+	case ']':
+		tok = token.Token{Type: "DELIMITER_CLOSE_BRACKET"}
+	case '(':
+		tok = token.Token{Type: "DELIMITER_OPEN_PARENTHESES"}
+	case ')':
+		tok = token.Token{Type: "DELIMITER_CLOSE_PARENTHESES"}
+	case ';':
+		tok = token.Token{Type: "DELIMITER_SEMICOLON"}
+	case '=':
+		tok = token.Token{Type: "OPERATOR_ASSIGN"}
+	case '+':
+		tok = token.Token{Type: "OPERATOR_ADDITION"}
+	case '-':
+		tok = token.Token{Type: "OPERATOR_SUBTRACTION"}
+	case '*':
+		tok = token.Token{Type: "OPERATOR_MULTIPLICATION"}
+	case '/':
+		tok = token.Token{Type: "OPERATOR_DIVISION"}
+	}
+	if tok.Type != "" {
+		found = true
+	}
+	return tok, found
 }
 
 // TODO: having environnment for functions and variables as maps for checking
@@ -48,31 +48,81 @@ func scanDelimiterAndOperator(b byte) (Token.Token, bool) {
 //       if the foo variable is used without existing then an error will occur
 //       if not it will just access it, same thing for functions
 
-func ScanFile(flags map[string]arguments.Flag, file arguments.File) []Token.Token {
-	var tokens []Token.Token
-	isReadingString := false
-	var token string = ""
+func cutWord(indexSrc int, src []byte, lenSrc int) (int, string) {
+	buffer := ""
+	finalIndex := indexSrc
 
-	for fileIndex, b := range file.Src {
-		if isReadingString == false {
-			tok, ok := scanDelimiterAndOperator(b)
-			if ok {
-				tokens = append(tokens, tok)
-			}
+	for finalIndex < lenSrc {
+		_, stop := scanDelimiterAndOperator(src[finalIndex])
+		if src[finalIndex] == ' ' || src[finalIndex] == '"' || src[finalIndex] == '\n' || stop {
+			break
 		}
-		if b == '"' {
+		buffer += string(src[finalIndex])
+		finalIndex++
+	}
+	return finalIndex, buffer
+}
+
+func scanKeyword(str string) (token.Token, bool) {
+	var tok token.Token
+	found := false
+
+	switch str {
+	case "let":
+		tok = token.Token{Type: "DECLARATION_LET"}
+	case "const":
+		tok = token.Token{Type: "DECLARATION_CONST"}
+	case "fn":
+		tok = token.Token{Type: "DECLARATION_FN"}
+	case "if":
+		tok = token.Token{Type: "STATEMENT_IF"}
+	case "while":
+		tok = token.Token{Type: "STATEMENT_WHILE"}
+	}
+
+	if tok.Type != "" {
+		found = true
+	}
+	return tok, found
+}
+
+func ScanFile(flags map[string]arguments.Flag, file arguments.File) []token.Token {
+	var tokens []token.Token
+	isReadingString := false
+	var tmpToken string = ""
+	lenSrc := len(file.Src)
+
+	for fileIndex := 0; fileIndex < lenSrc; fileIndex++ {
+		if file.Src[fileIndex] == '"' {
 			if isReadingString == false {
 				isReadingString = true
 				continue
 			} else if fileIndex-1 > 0 && file.Src[fileIndex-1] != '\\' {
-				tokens = append(tokens, Token.Token{Type: "VALUE_STRING", Value: token})
-				token = ""
+				tokens = append(tokens, token.Token{Type: "VALUE_STRING", Value: tmpToken, HasValue: true})
+				tmpToken = ""
 				isReadingString = false
 				continue
 			}
 		}
 		if isReadingString {
-			token += string(b)
+			tmpToken += string(file.Src[fileIndex])
+		}
+		if isReadingString == false {
+			tok, ok := scanDelimiterAndOperator(file.Src[fileIndex])
+			if ok {
+				tokens = append(tokens, tok)
+			} else {
+				idx, word := cutWord(fileIndex, file.Src, lenSrc)
+				if idx != fileIndex {
+					fmt.Printf("Found word = [%s]\n", word)
+					fileIndex = idx
+					tok, found := scanKeyword(word)
+					if found {
+						tokens = append(tokens, tok)
+					}
+				}
+				continue
+			}
 		}
 	}
 
@@ -80,7 +130,6 @@ func ScanFile(flags map[string]arguments.Flag, file arguments.File) []Token.Toke
 		for _, t := range tokens {
 			debugger.PrintToken(t)
 		}
-		fmt.Println()
 	}
 
 	return tokens
